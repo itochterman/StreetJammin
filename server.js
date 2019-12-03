1cc114e56e62712938ece7f4fb74cf0d09262493
Isabella <irt221@nyu.edu>
2019-12-02 22:38:16 -0500

    more config

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
